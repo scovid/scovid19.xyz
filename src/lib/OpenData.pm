9191923d0c11604af9f024b2513225219f5b8151
Dan Stewart <git@mail.danstewart.dev>
2020-09-09 16:45:38 +0100

    Add comments for data types

--- src/lib/OpenData.pm
+++ src/lib/OpenData.pm
@@ -9,10 +9,10 @@ use Carp qw{carp};
 
 my %resources = (
 	councils             => '967937c4-8d67-4f39-974f-fd58c4acfda5',
-	daily                => '287fc645-4352-4477-9c8c-55bc054b7e76',
-	total_by_area        => 'e8454cf0-1152-4bcb-b9da-4343f625dfef',
-	daily_by_area        => '427f9a25-db22-4014-a3bc-893b68243055',
-	total_by_deprivation => 'a965ee86-0974-4c93-bbea-e839e27d7085',
+	daily                => '287fc645-4352-4477-9c8c-55bc054b7e76', # Daily and Cumulative Cases
+	total_by_area        => 'e8454cf0-1152-4bcb-b9da-4343f625dfef', # Total Cases By Council Area
+	daily_by_area        => '427f9a25-db22-4014-a3bc-893b68243055', # Daily Case Trends By Council Area
+	total_by_deprivation => 'a965ee86-0974-4c93-bbea-e839e27d7085', # Total Cases By Deprivation
 );
 
 sub new {
