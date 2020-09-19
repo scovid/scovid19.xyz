f7fc6809c919d0e830bddc55ca863f99c72a305e
Dan Stewart <git@mail.danstewart.dev>
2020-09-19 10:56:41 +0100

    Fix gitignore

---
+++ src/lib/OpenData.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import logging
+logger = logging.getLogger(__name__)
+
+class OpenData:
+	endpoint = "https://www.opendata.nhs.scot/en/api/3/action/datastore_search"
+
+	resources = {
+		'councils':             '967937c4-8d67-4f39-974f-fd58c4acfda5',
+		'daily':                '287fc645-4352-4477-9c8c-55bc054b7e76', # Daily and Cumulative Cases
+		'total_by_area':        'e8454cf0-1152-4bcb-b9da-4343f625dfef', # Total Cases By Council Area
+		'daily_by_area':        '427f9a25-db22-4014-a3bc-893b68243055', # Daily Case Trends By Council Area
+		'total_by_deprivation': 'a965ee86-0974-4c93-bbea-e839e27d7085', # Total Cases By Deprivation
+	}
+
+	@staticmethod
+	def fetch(resource, **kwargs):
+		if resource not in OpenData.resources:
+			logger.error("ERROR: Requested resource '{}' is not valid".format(resource))
+			return {}
+
+		kwargs['resource_id'] = OpenData.resources[resource]
+		r = requests.get(OpenData.endpoint, params=kwargs)
+
+		if r.status_code != 200:
+			logger.error("ERROR: Bad response from OpenData API:\n{}".format(r.content))
+			return {}
+
+		json = r.json()
+		return json['result']
---
+++ src/lib/SCOVID.py
@@ -0,0 +1,226 @@
+from lib.OpenData import OpenData
+from datetime import datetime
+from collections import defaultdict
+
+# TODO: Handle OpenData errors
+
+class SCOVID:
+	# Get the mapping of council IDs to council names
+	@staticmethod
+	def councils():
+		councils = OpenData.fetch('councils')
+		return { council['CA']: council['CAName'] for council in councils['records'] }
+
+
+	# Return the summary of stats
+	@staticmethod
+	def summary():
+		cases_by_day = OpenData.fetch('daily', limit=1000, sort='Date ASC')
+		records = cases_by_day['records']
+
+		summary = {
+			'cases': {
+				'total': records[-1]['CumulativeCases'],
+				'today': records[-1]['DailyCases']
+			},
+			'deaths': {
+				'total': records[-1]['Deaths'],
+				'today': records[-1]['Deaths'] - records[-2]['Deaths']
+			}
+		}
+
+		max_deaths = {}
+		max_cases = {}
+		prev_day = {}
+		for day in records:
+			new_deaths = day['Deaths'] - prev_day['Deaths'] if prev_day else day['Deaths']
+
+			if not max_deaths or new_deaths > max_deaths['number']:
+				max_deaths['number'] = day['Deaths']
+				max_deaths['date'] = day['Date']
+
+			if not max_cases or day['DailyCases'] > max_cases['number']:
+				# On Apr 19th the stats started to include UK test centres
+				# So this day isn't really an accurate representation
+				if day['Date'] != '20200420':
+					max_cases['number'] = day['DailyCases']
+					max_cases['date']   = day['Date']
+
+			prev_day = day
+
+		summary['cases']['most']  = max_cases
+		summary['deaths']['most'] = max_deaths
+		return summary
+
+
+	# Return the overall cases by day for Scotland
+	@staticmethod
+	def trend(params={}):
+
+		limit, offset = 30, 0
+
+		if 'start' in params and 'end' in params:
+			start = datetime.strptime(params['start'], '%Y-%m-%d')
+			end = datetime.strptime(params['end'], '%Y-%m-%d')
+			last_updated = SCOVID.last_updated()
+
+			limit = (end - start).days + 1 # Add one to make bounds inclusive
+			offset = (last_updated - end).days
+
+		trend = OpenData.fetch('daily', sort='Date DESC', limit=limit, offset=offset)
+		records = reversed(trend['records'])
+
+		dates = []
+		cases = []
+		for day in records:
+			# TODO: Fix date format
+			dates.append(day['Date'])
+			cases.append(day['DailyCases'])
+
+		return {
+			'labels': dates,
+			'datasets': [{
+				'backgroundColor': 'darkorange',
+				'label': 'Positive',
+				'data': cases
+			}]
+		}
+
+
+	@staticmethod
+	def breakdown():
+		positive, negative, deaths = 0, 0, 0
+
+		# There doesn't seem to be a good endpoint for getting total negatives
+		# So use total_by_deprivation and total it manually
+		total_by_deprivation = OpenData.fetch('total_by_deprivation')
+		records = total_by_deprivation['records']
+		for record in records:
+			positive += record['TotalPositive']
+			negative += record['TotalNegative']
+			deaths   += record['TotalDeaths']
+
+		return {
+			'labels': ['Positive', 'Negative', 'Deaths'],
+			'datasets': [{
+				'backgroundColor': ['darkorange', 'lightgreen', 'darkgrey'],
+				'borderColor': ['darkorange', 'lightgreen', 'darkgrey'],
+				'label': 'Breakdown',
+				'data': [ positive, negative, deaths ]
+			}]
+		}
+
+
+	@staticmethod
+	def locations_total():
+		total_by_area = OpenData.fetch('total_by_area')
+		records = total_by_area['records']
+
+		councils = SCOVID.councils()
+
+		sets = []
+		for location in records:
+			sets.append({
+				'x': councils[location['CA']],
+				'y': location['TotalCases']
+			})
+
+		sets = sorted(sets, key=lambda k: k['x'])
+		return {
+			'labels': sorted(set(councils.values())),
+			'datasets': [{
+				'backgroundColor': [ SCOVID.color(item['x']) for item in sets ],
+				'label': 'Cases by area',
+				'data': sets
+			}]
+		}
+
+
+	@staticmethod
+	def locations_new():
+		councils = SCOVID.councils()
+		daily_by_area = OpenData.fetch('daily_by_area', limit=(len(councils) * 7), sort='Date DESC')
+		records = daily_by_area['records']
+
+		totals = defaultdict(lambda: 0)
+		for record in records:
+			totals[record['CA']] += record['DailyPositive']
+
+		sets = []
+		for ca in totals.keys():
+			if not ca or ca not in councils:
+				continue
+
+			sets.append({
+				'x': councils[ca],
+				'y': totals[ca]
+			})
+
+		sets = sorted(sets, key=lambda k: k['x'])
+		return {
+			'labels': sorted(set(councils.values())),
+			'datasets': [{
+				'backgroundColor': [ SCOVID.color(item['x']) for item in sets ],
+				'label': 'Cases by area',
+				'data': sets
+			}]
+		}
+
+
+	# Get the last updated time of the OpenData stats
+	# Based on the latest date in the "Daily and Cumulative Cases" data set
+	@staticmethod
+	def last_updated():
+		cases_by_day = OpenData.fetch('daily', limit=1, sort='Date DESC')
+		records = cases_by_day['records']
+		return datetime.strptime(str(records[-1]['Date']), '%Y%m%d')
+
+
+	@staticmethod
+	def color(key):
+		key = key.strip()
+
+		color_map = {
+			# Types
+			'deaths': '#b3b3b3',
+			'negative': '#b2df8a',
+			'positive': 'ff7f00',
+
+			# Locations
+			'Aberdeen City': '#33a02c',
+			'Aberdeenshire': '#1f78b4',
+			'Angus': '#b2df8a',
+			'Argyll and Bute': '#a6cee3',
+			'City of Edinburgh': '#fb9a99',
+			'Clackmannanshire': '#e31a1c',
+			'Dumfries and Galloway': '#fdbf6f',
+			'Dundee City': '#ff7f00',
+			'East Ayrshire': '#cab2d6',
+			'East Dunbartonshire': '#6a3d9a',
+			'East Lothian': '#ffff99',
+			'East Renfrewshire': '#b15928',
+			'Falkirk': '#66c2a5',
+			'Fife': '#a6d854',
+
+			# TODO: These need nicer colors
+			'Glasgow City': 'orangered',
+			'Highland': 'blue',
+			'Inverclyde': 'purple',
+			'Midlothian': 'cyan',
+			'Moray': 'pink',
+			'Na h-Eileanan Siar': 'burgandy',
+			'North Ayrshire': 'red',
+			'North Lanarkshire': 'yellow',
+			'Orkney Islands': 'lime',
+			'Perth and Kinross': 'rebeccapurple',
+			'Renfrewshire': 'brown',
+			'Scottish Borders': 'lightblue',
+			'Shetland Islands': 'navy',
+			'South Ayrshire': 'green',
+			'South Lanarkshire': 'orange',
+			'Stirling': 'darkgreen',
+			'West Dunbartonshire': 'grey',
+			'West Lothian': 'magenta',
+		}
+
+		return color_map[key] if key in color_map else 'yellow'
