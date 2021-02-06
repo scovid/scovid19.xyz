410d901918b7d841f922d010ac703cde0dd321be
Dan Stewart <git@mail.danstewart.dev>
2021-02-06 13:18:52 +0000

    Fix control.sh deploy

--- control.sh
+++ control.sh
@@ -127,14 +127,14 @@ if [[ -n $docker ]]; then
 		# Kill the old container, then scale down to 1
 		echo "Scaling up $name (NOTE: You can ignore the following two warnings about the container name and port)"
 		docker rename $name "${name}_old"
-		docker-compose up -d --scale app=2 --no-recreate app
+		docker-compose up -d --scale app=2 --no-recreate $app_name
 
 		echo "Building new container"
 		sleep 20
 
 		echo "New container built, removing old one"
 		docker rm -f "${name}_old"
-		docker-compose up -d --scale app=1 --no-recreate
+		docker-compose up -d --scale app=1 --no-recreate $app_name
 		echo "Deploy finished"
 
 	else
