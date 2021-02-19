4767b083aaa0d5ae42341842dfe645d52d508f64
danstewart <danstewart@users.noreply.github.com>
2021-02-19 19:02:17 +0000

    :art: Auto-format with tan

--- src/scripts/scraper.py
+++ src/scripts/scraper.py
@@ -16,28 +16,32 @@ logging.basicConfig(
 	format="[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
 )
 
-URL='https://www.gov.scot/publications/coronavirus-covid-19-daily-data-for-scotland/'
+URL = "https://www.gov.scot/publications/coronavirus-covid-19-daily-data-for-scotland/"
+
 
 def main():
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-	html = requests.get(URL, headers = headers)
-	parsed = BeautifulSoup(html.text, 'html.parser')
+	headers = {
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+	}
+	html = requests.get(URL, headers=headers)
+	parsed = BeautifulSoup(html.text, "html.parser")
 
 	summary = get_summary(parsed)
 
 	dose1 = get_first_doses(summary)
 	dose2 = get_second_doses(summary)
 
-	date = datetime.today().strftime('%Y-%m-%d')
+	date = datetime.today().strftime("%Y-%m-%d")
 
-	content = json.dumps({ 'date': date, 'dose1': dose1, 'dose2': dose2 })
+	content = json.dumps({"date": date, "dose1": dose1, "dose2": dose2})
 
 	write_file(content)
 
 	return
 
+
 def write_file(content):
-	filepath = 'data/vaccine.json'
+	filepath = "data/vaccine.json"
 
 	fh = open(filepath, "a")
 	fh.write(content)
@@ -47,14 +51,19 @@ def write_file(content):
 
 
 def get_summary(parsed):
-	summary = parsed.find(class_="body-content publication-body").findAll('ul')[0].findAll('li')[5]
+	summary = (
+		parsed.find(class_="body-content publication-body").findAll("ul")[0].findAll("li")[5]
+	)
 
 	return summary.find_all_next(string=True)
+
 
 def get_first_doses(summary):
 	return summary[0]
 
+
 def get_second_doses(summary):
 	return summary[3]
+
 
 main()
