1b7dbb959c7ef004fc26bd55743a7d7111f98bbc
Craig McAulay <craigmcaulay98@googlemail.com>
2021-02-22 20:19:47 +0000

    New script to tweet daily stats

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,6 +3,7 @@
 # Start cron
 sudo service cron start
 (crontab -l 2>/dev/null; echo "0 15 * * * /usr/bin/env python3 /home/code/scovid19/src/scripts/scraper.py") | crontab -
+(crontab -l 2>/dev/null; echo "5 15 * * * /usr/bin/env python3 /home/code/scovid19/src/scripts/tweet.py") | crontab -
 
 # Start web server
 ./control.sh --env $ENV --flask up
---
+++ src/scripts/tweet.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import tweepy
+import logging
+
+from datetime import datetime
+
+sys.path.append('../')
+
+from lib.Vaccine import Vaccine
+from lib.Infections import Infections
+
+logging.basicConfig(
+	filename="/home/code/scovid19/logs/bot.log",
+	level=logging.INFO,
+	format="[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+)
+
+#Add your credentials here
+twitter_keys = {
+        'consumer_key':        os.environ['SCOVID_TWITTER_API_KEY'],
+        'consumer_secret':     os.environ['SCOVID_TWITTER_API_SECRET'],
+        'access_token_key':    os.environ['SCOVID_TWITTER_ACCESS_TOKEN'],
+        'access_token_secret': os.environ['SCOVID_TWITTER_ACCESS_SECRET']
+    }
+
+vaccine = Vaccine()
+infections = Infections()
+
+auth = tweepy.OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
+auth.set_access_token(twitter_keys['access_token_key'], twitter_keys['access_token_secret'])
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+	wait_on_rate_limit_notify=True)
+
+vaccine_data = vaccine.get_scraper_data()
+infection_data = infections.summary()
+
+daily_cases = f"{infection_data['cases']['today']:,.0f}"
+
+date = datetime.today().strftime("%Y-%m-%d")
+
+v = { "date" : date, "dose1": f"{vaccine_data['dose1']:,.0f}", "dose2":  f"{vaccine_data['dose2']:,.0f}", "daily_cases": daily_cases }
+text = "Scotland vaccinations as of 08:30am on {date}: \n\n First Dose: {dose1} \n Second Dose: {dose2} \n\n {daily_cases} new infection cases confirmed \n\n For more stats visit www.scovid19.xyz"
+
+try:
+	api.update_status(text.format(**v))
+except tweepy.TweepError as e:
+	logging.error("Error: " + e.response.text)
+	sys.exit(1)
+except tweepy.RateLimitError as e:
+	logging.error("Error: " + e.response.text)
+	sys.exit(1)
+except:
+	logging.error("Unknown Error.")
+	sys.exit(1)
