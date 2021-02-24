431715a07f8659b5edde2a4ce76c53097c129f6e
Dan Stewart <git@mail.danstewart.dev>
2021-02-24 15:58:14 +0000

    Fix cron

--- scovid19/entrypoint.sh
+++ scovid19/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Start cron
 sudo service cron start
-(crontab -l 2>/dev/null; echo "0 15 * * * cd $PROJECT_ROOT && /usr/bin/env python3 -m scovid19.scripts.scraper") | crontab -
-(crontab -l 2>/dev/null; echo "5 15 * * * cd $PROJECT_ROOT && /usr/bin/env python3 -m scovid19.scripts.tweet") | crontab -
+(crontab -l 2>/dev/null; echo "0 15 * * * bash -c 'cd $PROJECT_ROOT && source $PROJECT_ROOT/secrets.bash && /usr/bin/env python3 -m scovid19.scripts.scraper'") | crontab -
+(crontab -l 2>/dev/null; echo "5 15 * * * bash -c 'cd $PROJECT_ROOT && source $PROJECT_ROOT/secrets.bash && /usr/bin/env python3 -m scovid19.scripts.tweet'") | crontab -
 
 # Start web server
 ./control.sh --env $ENV --flask up
